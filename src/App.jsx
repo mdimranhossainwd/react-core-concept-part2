4fe66c1825a4671e25b1ca69badab6dc4d21a2a6
Md.Imran <mdimranhossain7498@gmail.com>
2023-09-08 11:52:57 +0600

    Counter Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Data from "./Data";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
@@ -16,6 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <Data></Data>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
